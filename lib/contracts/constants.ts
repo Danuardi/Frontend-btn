c92a548ab98bdbe6e17335fac2b1de198c2b3e63
Syahlevi Aldarna <ruchapnara@gmail.com>
2025-06-04 20:14:38 +0700

    fix: switch from mockUSDC to base sepolia testnet

--- lib/contracts/constants.ts
+++ lib/contracts/constants.ts
@@ -1,10 +1,10 @@
-import { Address } from "viem";
+import { Address, Abi } from "viem";
 import POSITIONS_ABI from "./POSITIONS_ABI.json";
 
 export const RPC_URL = `https://backend.mrfql.my.id`;
 
-export const POSITION_ABI = POSITIONS_ABI;
+export const POSITION_ABI = POSITIONS_ABI as Abi;
 
 export const POSITION_ADDRESS: Record<number, Address> = {
-  84532: "0xB8ACbfeeacf053E85B51D71563A95d3EEcF57984", // Base Sepolia
+  84532: "0x34662e1BE68A95141550c69c4aD7844EA2314b0D", // Base Sepolia
 } as const;
